7246e1173f1087a57b37f5e7b8bbee9df6a9f991
Mark Bestley <git@bestley.co.uk>
2008-06-01 20:40:51 +0100

    comment out the vc backends as it is now in emacs

--- common-setup.el
+++ common-setup.el
@@ -1,7 +1,7 @@
 ;; git
 
  (require 'vc-git)
- (when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
+;; (when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
  (require 'git)
  (autoload 'git-blame-mode "git-blame"
            "Minor mode for incremental blame for Git." t)

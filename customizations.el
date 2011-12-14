ec361cf3f8f6807ed46bcc81b658f079ee3034f3
Mark Bestley <git@bestley.co.uk>
2011-12-14 13:37:13 +0000

    Custom May 2011

--- customizations.el
+++ customizations.el
@@ -10,6 +10,10 @@
  '(delete-old-versions t)
  '(gnus-directory "~/Library/Application Support/Aquamacs Emacs/News")
  '(gnus-group-sort-function (quote (gnus-group-sort-by-rank)))
+ '(haskell-font-lock-symbols t)
+ '(haskell-hugs-program-args (quote ("+." "-98")))
+ '(haskell-mode-hook (quote (turn-on-haskell-indentation turn-on-font-lock imenu-add-menubar-index)))
+ '(haskell-program-name "ghci")
  '(make-backup-files t)
  '(ns-alternate-modifier (quote meta))
  '(ns-tool-bar-display-mode (quote both) t)
@@ -17,7 +21,7 @@
  '(nxml-slash-auto-complete-flag t)
  '(py-imenu-show-method-args-p t)
  '(py-python-command-args (quote ("-i" "-colors" "LightBG" "--autocall" "0")))
- '(python-mode-hook (quote (imenu-add-menubar-index my-python-mode-hook (lambda nil (define-key py-mode-map (kbd "M-<tab>") (quote anything-ipython-complete))))) t))
+ '(python-mode-hook (quote (imenu-add-menubar-index my-python-mode-hook (lambda nil (define-key py-mode-map (kbd "M-<tab>") (quote anything-ipython-complete)))))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.

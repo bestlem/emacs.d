995377459311b4afe275e75ec886284bbda8d351
Mark Bestley <git@bestley.co.uk>
2011-12-14 14:00:10 +0000

    Edit outside emacs to add back all but python

--- customizations.el
+++ customizations.el
@@ -9,10 +9,25 @@
  '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode (quote regular) t)
  '(one-buffer-one-frame-mode t nil (aquamacs-frame-setup))
+ '(auto-word-wrap-default-function nil)
+ '(delete-old-versions t)
+ '(gnus-directory "~/Library/Application Support/Aquamacs Emacs/News")
+ '(gnus-group-sort-function (quote (gnus-group-sort-by-rank)))
+ '(haskell-font-lock-symbols t)
+ '(haskell-hugs-program-args (quote ("+." "-98")))
+ '(haskell-mode-hook (quote (turn-on-haskell-indentation turn-on-font-lock imenu-add-menubar-index)))
+ '(haskell-program-name "ghci")
+ '(make-backup-files t)
+ '(ns-alternate-modifier (quote meta))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(nxml-slash-auto-complete-flag t)
+ '(py-imenu-show-method-args-p t)
+
  '(visual-line-mode nil t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Menlo")))))

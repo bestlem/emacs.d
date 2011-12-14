6648e9c722995180cbd70488fc9ddb8ef5db03be
Mark Bestley <git@bestley.co.uk>
2011-12-14 13:32:45 +0000

    Customisation as at dec 2011

---
+++ customizations.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 213 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(one-buffer-one-frame-mode t nil (aquamacs-frame-setup))
+ '(visual-line-mode nil t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )

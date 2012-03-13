81c4b4f1b9c7cb20b8ef245e53e3f74dfa7ef1ba
Mark Bestley <git@bestley.co.uk>
2012-03-13 11:06:06 +0000

    Try sql-mode - non working

--- common-modes.el
+++ common-modes.el
@@ -19,6 +19,20 @@
 ;; http://zagadka.vm.bytemark.co.uk/magit/magit.html
 ;(autoload 'magit-status "magit" nil t)
 
+
+;;; SQL
+;; (autoload 'sql "sql-mode"
+;;       "Start the interactive SQL interpreter in a new buffer." t)
+
+;; (autoload 'sql-mode "sql-mode"
+;;       "Mode for editing SQL files and running a SQL interpreter." t)
+
+;; (autoload 'sql-buffer "sql-mode"
+;;       "Create or move to the sql-mode \"*SQL commands*\" buffer." t)
+
+;; (setq auto-mode-alist (cons '("\\.sql$" . sql-mode) auto-mode-alist))
+
+
 ;; objective C
 (add-to-list 'auto-mode-alist '("\\.h$" . objc-mode) )
 (add-to-list 'auto-mode-alist '("\\.m$" . objc-mode))

a3ccd748e6668045c4004865fa79b99f8dc414ce
Mark Bestley <git@bestley.co.uk>
2014-05-06 20:05:18 +0100

    Haskell and packages

--- customizations.el
+++ customizations.el
@@ -1,8 +1,8 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 213 t)
  '(aquamacs-tool-bar-user-customization nil t)
@@ -13,7 +13,9 @@
  '(gnus-group-sort-function (quote (gnus-group-sort-by-rank)))
  '(haskell-font-lock-symbols t)
  '(haskell-hugs-program-args (quote ("+." "-98")))
- '(haskell-mode-hook (quote (turn-on-haskell-indentation turn-on-font-lock imenu-add-menubar-index)) t)
+ '(haskell-mode-hook
+   (quote
+	(turn-on-haskell-indentation turn-on-font-lock imenu-add-menubar-index)))
  '(haskell-program-name "ghci")
  '(mail-setup-with-from t)
  '(make-backup-files t)
@@ -22,13 +24,19 @@
  '(ns-tool-bar-size-mode (quote regular) t)
  '(nxml-slash-auto-complete-flag t)
  '(one-buffer-one-frame-mode nil nil (aquamacs-frame-setup))
+ '(package-archive-upload-base "/Users/mark/.cache/emacs-package")
+ '(package-archives
+   (quote
+	(("gnu" . "http://elpa.gnu.org/packages/")
+	 ("marmalade" . "http://marmalade-repo.org/packages/"))))
+ '(py-imenu-create-index-p t)
  '(py-imenu-show-method-args-p t)
  '(tabbar-mode t nil (tabbar))
  '(visual-line-mode nil t)
  '(x-select-enable-clipboard t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Menlo")))))

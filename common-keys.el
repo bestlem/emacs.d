859f8f77b9ff1507266a59931094b2265208f2d4
Mark Bestley <37385+bestlem@users.noreply.github.com>
2019-03-30 15:34:00 +0000

    Put keys all in one file

--- common-keys.el
+++ common-keys.el
@@ -1,5 +1,15 @@
 ;; $Header: /Users/mark/Library/Emacs/RCS/common-keys.el,v 1.1.1.7 2006/07/23 12:24:49 mark Exp mark $
 
+;; ; keys
+(define-key osx-key-mode-map [end] 'end-of-line )
+(define-key osx-key-mode-map [home] 'beginning-of-line )
+;;(define-key osx-key-mode-map [C-end] 'end-of-buffer )
+(define-key osx-key-mode-map [C-home] 'beginning-of-buffer )
+(define-key osx-key-mode-map [C-kp-end] 'end-of-buffer )
+(define-key osx-key-mode-map [C-kp-home] 'beginning-of-buffer )
+(define-key osx-key-mode-map [S-kp-next] 'scroll-other-window-down )
+(define-key osx-key-mode-map [S-kp-prior] 'scroll-other-window )
+
 					; keys
 (global-set-key [end] 'end-of-line )
 (global-set-key [home] 'beginning-of-line )

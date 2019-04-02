7ee09220c684115f04c7a09a75a7edd439a3e3f9
Mark Bestley <37385+bestlem@users.noreply.github.com>
2019-04-02 22:55:28 +0100

    Extra general behaviour

--- common-setup.el
+++ common-setup.el
@@ -41,6 +41,15 @@
 (setq-default tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100))
 
 
+
+;; from http://ergoemacs.org/emacs/emacs_make_modern.html but some are in aquamacs
+;; UTF-8 as default encoding
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+(setq backup-by-copying t)
+
+
 ;; GNUS
 ; gmail
 (require 'gnus )

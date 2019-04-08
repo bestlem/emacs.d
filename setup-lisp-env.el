0cfa47962fec23b76e474df5c5a7d1276b9115b7
Mark Bestley <37385+bestlem@users.noreply.github.com>
2019-04-08 16:44:02 +0100

    lisp added

--- setup-lisp-env.el
+++ setup-lisp-env.el
@@ -11,3 +11,13 @@
   :ensure t
   :hook (elisp-mode . aggresive-indent-mode)
   )
+
+(use-package
+  lispy
+  :ensure t
+  :config (add-hook
+		   'emacs-lisp-mode-hook
+		   (lambda () (lispy-mode 1)))
+  (add-hook
+   'lisp-interaction-mode-hook
+   (lambda () (lispy-mode 1))))

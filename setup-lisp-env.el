47535fe95c0e6c61d7b9156a919a6995f7806cb8
Mark Bestley <37385+bestlem@users.noreply.github.com>
2019-04-13 12:57:42 +0100

    Add elisp format (and naughtily use it)

--- setup-lisp-env.el
+++ setup-lisp-env.el
@@ -1,23 +1,25 @@
 ;; Well just elisp for new
 
 ;; Make parens pretty
-(use-package rainbow-delimiters
+(use-package
+  rainbow-delimiters
   :ensure t
   :delight rainbow-delimiters-mode
-  :hook (prog-mode . rainbow-delimiters-mode)
-  )
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-(use-package aggressive-indent
+(use-package
+  aggressive-indent
   :ensure t
-  :hook (elisp-mode . aggresive-indent-mode)
-  )
+  :hook (emacs-lisp-mode . aggresive-indent-mode))
 
 (use-package
   lispy
   :ensure t
-  :config (add-hook
-		   'emacs-lisp-mode-hook
-		   (lambda () (lispy-mode 1)))
-  (add-hook
-   'lisp-interaction-mode-hook
-   (lambda () (lispy-mode 1))))
+  :config (add-hook 'emacs-lisp-mode-hook (lambda ()
+											(lispy-mode 1)))
+  (add-hook 'lisp-interaction-mode-hook (lambda ()
+										  (lispy-mode 1))))
+
+(use-package
+  elisp-format
+  :ensure t)

09f1d1fa20ae59542ce4100cfcd090a4b7e3d029
Mark Bestley <37385+bestlem@users.noreply.github.com>
2019-04-17 20:21:57 +0100

    Various emacs behaviours

--- common-setup.el
+++ common-setup.el
@@ -56,8 +56,50 @@
   :ensure t
   :bind ("C-=" . er/expand-region))
 
+;;  Sentances do not end with two spaces
+(setq sentence-end-double-space nil)
+
+;; Show keyboard macro as elisp
+(use-package elmacro :ensure t)
 (load "setup-file-management")
 (load "setup-gnus")
 
 
 (load "setup-apperance")
+
+(progn
+  ;; General emacs behaviour
+  ;;  From https://sriramkswamy.github.io/dotemacs/
+
+  ;; This package helps to discover the major mode bindings. I use it very occasionally and hence not binding it to any modal binding.
+  (use-package discover-my-major
+	:ensure t
+	:bind (("C-h C-m" . discover-my-major)
+		   ("C-h M-m" . discover-my-mode)))
+
+  ;; smex is an amazing program that helps order the M-x commands based on usage and recent items. Let’s install it.
+  (use-package smex
+	:ensure t
+	:config
+	(smex-initialize))
+
+  ;; comment-dwim-2 improves on the existing comment-dwim command for easy commenting. Pretty useful.
+
+  (use-package comment-dwim-2
+	:ensure t
+	:bind* ("M-;" . comment-dwim-2))
+)
+;;  Hide show etc
+(use-package
+  origami
+  :ensure t
+  :commands (origami-toggle-node)
+  :bind* (("M-m -" . orgiami-toggle-node)
+		  ("C-c TAB" . origami-recursively-toggle-node)
+		  ("C-\\" . origami-recursively-toggle-node)
+		  ("M-\\" . origami-close-all-nodes)
+		  ("M-+" . origami-open-all-nodes)))
+
+
+;;  Completer
+;; From https://pages.sachachua.com/.emacs.d/Sacha.html#org04e47b9

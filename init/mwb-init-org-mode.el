cd2b8ea88e838b9073ee3533a283042870c1736e
Mark Bestley <37385+bestlem@users.noreply.github.com>
2019-05-12 19:58:42 +0100

    Various formatting

--- init/mwb-init-org-mode.el
+++ init/mwb-init-org-mode.el
@@ -4,35 +4,34 @@
 
 ;; [[file:~/Library/Preferences/Emacs/mwb-init-org-mode.org::*The%20use%20package%20setup][The use package setup:1]]
 (use-package org
-   :ensure t
-   :hydra (hydra-org-mode
-		   (:color teal)
-		   ""
-		   ("c" mwb-hydra-org-code/body "Code")
-		   ("d" org-toggle-link-display "Show links")
-		   ("l" org-insert-link "link")
-		   ("g" org-set-tags-command "tags")
-		   ("t" org-todo "todo")
-		   ("q" nil "cancel"))
-   :after hydra
-   :bind (:map org-mode-map
-			   ("H-a" . hydra-org-mode/body)
-			   ("H-s" . org-edit-special)
-			   :map org-src-mode-map
-			   ("H-s" . org-edit-src-exit)
-			   ("A-s" . org-edit-src-exit) ; I press by mistake but it makes sense
-			   )
-   :hook
-(org-mode . (lambda ()
-					(add-hook
-					 'after-save-hook
-					 'org-babel-tangle
-					 'run-at-end
-					 'only-in-org-mode)))
-(org-mode . visual-line-mode)
-(org-mode . variable-pitch-mode)
-
-   )
+  :ensure t
+  :hydra (hydra-org-mode
+		  (:color teal)
+		  ""
+		  ("c" mwb-hydra-org-code/body "Code")
+		  ("d" org-toggle-link-display "Show links")
+		  ("l" org-insert-link "link")
+		  ("g" org-set-tags-command "tags")
+		  ("t" org-todo "todo")
+		  ("q" nil "cancel"))
+  :after hydra
+  :bind (:map org-mode-map
+			  ("H-a" . hydra-org-mode/body)
+			  ("H-s" . org-edit-special)
+			  :map org-src-mode-map
+			  ("H-s" . org-edit-src-exit)
+			  ("A-s" . org-edit-src-exit) ; I press by mistake but it makes sense
+			  )
+  :hook
+      (org-mode . (lambda ()
+      					(add-hook
+      					 'after-save-hook
+      					 'org-babel-tangle
+      					 'run-at-end
+      					 'only-in-org-mode)))
+      (org-mode . visual-line-mode)
+      (org-mode . variable-pitch-mode)
+  )
 ;; The use package setup:1 ends here
 
 ;; [[file:~/Library/Preferences/Emacs/mwb-init-org-mode.org::*Imenu%20to%20provide%20info%20for%20treemacs%20and%20contextual%20menu][Imenu to provide info for treemacs and contextual menu:1]]

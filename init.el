da3a7dbf0a1dd4ed7890011de07f3d640bad75fa
Mark Bestley <37385+bestlem@users.noreply.github.com>
2019-12-08 13:46:14 +0000

    Deal with Emacs changing init.el

--- init.el
+++ init.el
@@ -11,6 +11,14 @@
 	  auto-window-vscroll nil)
 ;; Startup:1 ends here
 
+;; [[file:~/.emacs.d/config.org::*package][package:1]]
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+;; package:1 ends here
+
 ;; [[file:~/.emacs.d/config.org::*Debug%20flag][Debug flag:1]]
 (setq init-file-debug nil)
 ;; Debug flag:1 ends here

78a1b376e1a48ed93d773de07bbd5dacadacfc47
Mark Bestley <37385+bestlem@users.noreply.github.com>
2020-10-13 18:57:09 +0100

    Working emacs 27 themes, window/fram hydra, font sizing

--- custom.el
+++ custom.el
@@ -16,6 +16,8 @@
  '(cua-enable-cua-keys nil)
  '(cursor-type 'box)
  '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   '("d4a89e8d54783f8d45c2c68cc6641ea2427f563405fde1f083191b10746fe59f" default))
  '(delete-old-versions "keep them all")
  '(desktop-file-name-format 'tilde)
  '(explicit-shell-file-name "/opt/local/bin/fish")
@@ -94,7 +96,7 @@
  '(org-src-window-setup 'current-window)
  '(org-todo-keywords '((sequence "TODO" "|" "DONE" "LEAVE(@)")))
  '(package-selected-packages
-   '(company-prescient company-posframe which-key-posframe which-key-posframe-mode treemacs-persp mac-key-mode no-littering hydra s dash use-package which-key racket-mode elmacro lispy rainbow-delimiters flycheck smartparens highlight-indent-guides nov treemacs-magit treemacs-icons-dired treemacs-projectile treemacs ibuffer-vc projectile deadgrep ivy-prescient prescient counsel swiper ivy-rich ivy-hydra ivy yasnippet company helpful org smartparens-config all-the-icons major-mode-hydra eval-in-repl xr yasnippet-snippets outshine expand-region json-navigator mwim mwin mosey fish-mode sml-mode use-package-chords paredit dashboard clojure-mode cmake-mode cask-mode eros macrostep org-indent xah-elisp-mode validate hardhat beacon ripgrep gitignore-mode gitconfig-mode manual matcha use-package-hydra backup-each-save esup org-bullets ws-butlerz helm elisp-format origami comment-dwim-2 smex discover-my-major json-mode ranger dired-toggle aggressive-indent gradle-mode groovy-imports groovy-mode ws-butler diminish key-chord))
+   '(default-text-scale zoom-frm color-theme-modern paren-face flyspell-correct-popup company-prescient company-posframe which-key-posframe which-key-posframe-mode mac-key-mode hydra s dash use-package which-key racket-mode elmacro lispy rainbow-delimiters flycheck smartparens highlight-indent-guides nov treemacs-magit treemacs-icons-dired treemacs-projectile treemacs ibuffer-vc projectile deadgrep ivy-prescient prescient counsel swiper ivy-rich ivy-hydra ivy yasnippet company helpful org smartparens-config all-the-icons major-mode-hydra eval-in-repl xr yasnippet-snippets outshine expand-region json-navigator mwim fish-mode sml-mode use-package-chords cmake-mode cask-mode eros macrostep org-indent validate hardhat beacon gitignore-mode gitconfig-mode manual use-package-hydra backup-each-save org-bullets ws-butlerz helm elisp-format comment-dwim-2 smex discover-my-major json-mode dired-toggle aggressive-indent gradle-mode groovy-imports groovy-mode ws-butler diminish key-chord))
  '(python-shell-interpreter "python")
  '(text-mode-hook '(turn-on-flyspell set-word-wrap))
  '(undo-limit 800000)

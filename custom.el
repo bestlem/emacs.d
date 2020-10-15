04ef2e5ba3bbd22b2898967b7e23140e6fe0979b
Mark Bestley <37385+bestlem@users.noreply.github.com>
2020-10-15 15:13:14 +0100

    remove tabbar as it is aquamacs

--- custom.el
+++ custom.el
@@ -107,7 +107,6 @@
    (quote
     (fira-code-mode paren-face flyspell-correct-popup company-prescient company-posframe which-key-posframe which-key-posframe-mode mac-key-mode hydra s dash use-package which-key racket-mode elmacro lispy rainbow-delimiters flycheck smartparens highlight-indent-guides nov treemacs-magit treemacs-icons-dired treemacs-projectile treemacs ibuffer-vc projectile deadgrep ivy-prescient prescient counsel swiper ivy-rich ivy-hydra ivy yasnippet company helpful org smartparens-config all-the-icons major-mode-hydra eval-in-repl xr yasnippet-snippets outshine expand-region json-navigator mwim fish-mode sml-mode use-package-chords cmake-mode cask-mode eros macrostep org-indent validate hardhat beacon gitignore-mode gitconfig-mode manual use-package-hydra backup-each-save org-bullets ws-butlerz helm elisp-format comment-dwim-2 smex discover-my-major json-mode dired-toggle aggressive-indent gradle-mode groovy-imports groovy-mode ws-butler diminish key-chord)))
  '(python-shell-interpreter "python")
- '(tabbar-mode nil nil (tabbar))
  '(text-mode-hook (quote (set-word-wrap)))
  '(undo-limit 800000)
  '(visual-line-mode t t)

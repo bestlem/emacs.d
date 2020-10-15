c00fca1ea37b58928b870fd11b3c45cad2cf4e8e
Mark Bestley <37385+bestlem@users.noreply.github.com>
2020-10-15 11:46:44 +0100

    Checkpoint - something is adding lots

--- custom.el
+++ custom.el
@@ -10,20 +10,22 @@
  '(aquamacs-tool-bar-user-customization nil t)
  '(auto-fill-function nil t)
  '(auto-word-wrap-default-function nil)
- '(backup-directory-alist '(("." . ".~")))
+ '(backup-directory-alist (quote (("." . ".~"))))
  '(canlock-password "8ac28fef8ff386f996bee4f20f8b3c40bd1829d4")
  '(comment-auto-fill-only-comments t)
  '(cua-enable-cua-keys nil)
- '(cursor-type 'box)
+ '(cursor-type (quote box))
  '(custom-enabled-themes nil)
  '(custom-safe-themes
-   '("d4a89e8d54783f8d45c2c68cc6641ea2427f563405fde1f083191b10746fe59f" default))
+   (quote
+    ("d4a89e8d54783f8d45c2c68cc6641ea2427f563405fde1f083191b10746fe59f" default)))
  '(delete-old-versions "keep them all")
- '(desktop-file-name-format 'tilde)
+ '(desktop-file-name-format (quote tilde))
  '(explicit-shell-file-name "/opt/local/bin/fish")
- '(fill-nobreak-predicate '(fill-single-char-nobreak-p))
+ '(fill-nobreak-predicate (quote (fill-single-char-nobreak-p)))
  '(fringe-indicator-alist
-   '((continuation nil nil)
+   (quote
+    ((continuation nil nil)
      (truncation left-truncation right-truncation)
      (continuation left-continuation right-continuation)
      (overlay-arrow . right-triangle)
@@ -33,23 +35,26 @@
      (bottom bottom-left-angle bottom-right-angle top-right-angle top-left-angle)
      (top-bottom left-bracket right-bracket top-right-angle top-left-angle)
      (empty-line . empty-line)
-     (unknown . question-mark)) t)
- '(fringe-mode '(16 . 0) nil (fringe))
+     (unknown . question-mark))) t)
+ '(fringe-mode (quote (16 . 0)) nil (fringe))
  '(global-hl-line-mode t)
  '(gnus-directory "~/Library/Preferences/Aquamacs Emacs/News")
- '(gnus-group-sort-function '(gnus-group-sort-by-rank) t)
+ '(gnus-group-sort-function (quote (gnus-group-sort-by-rank)) t)
  '(gnus-kill-save-kill-file t)
  '(gnus-score-mimic-keymap nil)
  '(gnus-server-browse-in-group-buffer t)
  '(gnus-show-all-headers nil)
  '(gnus-verbose 10)
  '(hardhat-basename-protected-regexps
-   '("~\\'" "\\.lock\\'" "\\.ix\\'" "\\`test\\.out\\'" "-autoloads\\.el\\'" "\\`Desktop\\.ini\\'" "\\`META\\.yml\\'" "\\`MYMETA\\.yml\\'" "\\`TAGS\\'" "\\`Thumbs\\.db\\'" "\\`\\.dropbox\\'" "\\`\\.dropbox\\.cache\\'" "\\`\\.emacs\\.desktop\\'" "\\`\\.emacs\\.desktop\\.lock\\'" "\\.orig\\'" "\\.rej\\'" "\\.el\\.gz$"))
+   (quote
+    ("~\\'" "\\.lock\\'" "\\.ix\\'" "\\`test\\.out\\'" "-autoloads\\.el\\'" "\\`Desktop\\.ini\\'" "\\`META\\.yml\\'" "\\`MYMETA\\.yml\\'" "\\`TAGS\\'" "\\`Thumbs\\.db\\'" "\\`\\.dropbox\\'" "\\`\\.dropbox\\.cache\\'" "\\`\\.emacs\\.desktop\\'" "\\`\\.emacs\\.desktop\\.lock\\'" "\\.orig\\'" "\\.rej\\'" "\\.el\\.gz$")))
  '(hardhat-fullpath-protected-regexps
-   '("~/\\.emacs\\.d/elpa/" "~/\\.cpan/" "~/\\.cabal/" "~/perl5/perlbrew/" "~/\\.npm/" "~/\\.virtualenv/" "~/\\.virthualenv/" "~/\\.rvm/" "/[._]build/" "/\\.bzr/" "/\\.coverage/" "/\\.git/" "/\\.hg/" "/\\.rspec/" "/\\.sass-cache/" "/\\.svn/" "/_MTN/" "/_darcs/" "/CVS/" "/pm_to_blib/" "/RCS/" "/SCCS/" "/blib/" "/test_output/" "~/\\.emacs\\.d/\\.cask/" "~/\\.cask/" "/Applications/" "~/Library/Preferences/Aquamacs Emacs/Packages" "~/src/ThirdParty/"))
+   (quote
+    ("~/\\.emacs\\.d/elpa/" "~/\\.cpan/" "~/\\.cabal/" "~/perl5/perlbrew/" "~/\\.npm/" "~/\\.virtualenv/" "~/\\.virthualenv/" "~/\\.rvm/" "/[._]build/" "/\\.bzr/" "/\\.coverage/" "/\\.git/" "/\\.hg/" "/\\.rspec/" "/\\.sass-cache/" "/\\.svn/" "/_MTN/" "/_darcs/" "/CVS/" "/pm_to_blib/" "/RCS/" "/SCCS/" "/blib/" "/test_output/" "~/\\.emacs\\.d/\\.cask/" "~/\\.cask/" "/Applications/" "~/Library/Preferences/Aquamacs Emacs/Packages" "~/src/ThirdParty/")))
  '(ibuffer-expert t)
  '(ibuffer-fontification-alist
-   '((999
+   (quote
+    ((999
       (and buffer-file-name
            (buffer-modified-p))
       term-cyanbg)
@@ -72,33 +77,38 @@
       (memq major-mode ibuffer-help-buffer-modes)
       font-lock-comment-face)
      (35
-      (derived-mode-p 'dired-mode)
+      (derived-mode-p
+       (quote dired-mode))
       font-lock-function-name-face)
-     (0 1 term-blue)))
+     (0 1 term-blue))))
  '(ibuffer-help-buffer-modes
-   '(help-mode helpful-mode apropos-mode Info-mode Info-edit-mode))
+   (quote
+    (help-mode helpful-mode apropos-mode Info-mode Info-edit-mode)))
  '(indent-tabs-mode nil)
  '(mail-setup-with-from t)
  '(make-backup-files t)
  '(minibuffer-prompt-properties
-   '(read-only t cursor-intangible t face minibuffer-prompt cursor-intangible t))
+   (quote
+    (read-only t cursor-intangible t face minibuffer-prompt cursor-intangible t)))
  '(ns-right-alternate-modifier nil)
- '(ns-right-command-modifier 'super)
- '(ns-tool-bar-display-mode 'both t)
+ '(ns-right-command-modifier (quote super))
+ '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode nil t)
  '(nxml-slash-auto-complete-flag t)
- '(org-babel-python-mode 'python-mode)
- '(org-bullets-bullet-list '("⦿" "◉" "◎" "○" "●"))
- '(org-datetree-add-timestamp 'active t)
- '(org-log-done 'time)
- '(org-modules '(org-crypt org-tempo org-id org-mouse))
+ '(org-babel-python-mode (quote python-mode))
+ '(org-bullets-bullet-list (quote ("⦿" "◉" "◎" "○" "●")))
+ '(org-datetree-add-timestamp (quote active) t)
+ '(org-log-done (quote time))
+ '(org-modules (quote (org-crypt org-tempo org-id org-mouse)))
  '(org-src-ask-before-returning-to-edit-buffer nil)
- '(org-src-window-setup 'current-window)
- '(org-todo-keywords '((sequence "TODO" "|" "DONE" "LEAVE(@)")))
+ '(org-src-window-setup (quote current-window))
+ '(org-todo-keywords (quote ((sequence "TODO" "|" "DONE" "LEAVE(@)"))))
  '(package-selected-packages
-   '(default-text-scale zoom-frm color-theme-modern paren-face flyspell-correct-popup company-prescient company-posframe which-key-posframe which-key-posframe-mode mac-key-mode hydra s dash use-package which-key racket-mode elmacro lispy rainbow-delimiters flycheck smartparens highlight-indent-guides nov treemacs-magit treemacs-icons-dired treemacs-projectile treemacs ibuffer-vc projectile deadgrep ivy-prescient prescient counsel swiper ivy-rich ivy-hydra ivy yasnippet company helpful org smartparens-config all-the-icons major-mode-hydra eval-in-repl xr yasnippet-snippets outshine expand-region json-navigator mwim fish-mode sml-mode use-package-chords cmake-mode cask-mode eros macrostep org-indent validate hardhat beacon gitignore-mode gitconfig-mode manual use-package-hydra backup-each-save org-bullets ws-butlerz helm elisp-format comment-dwim-2 smex discover-my-major json-mode dired-toggle aggressive-indent gradle-mode groovy-imports groovy-mode ws-butler diminish key-chord))
+   (quote
+    (fira-code-mode paren-face flyspell-correct-popup company-prescient company-posframe which-key-posframe which-key-posframe-mode mac-key-mode hydra s dash use-package which-key racket-mode elmacro lispy rainbow-delimiters flycheck smartparens highlight-indent-guides nov treemacs-magit treemacs-icons-dired treemacs-projectile treemacs ibuffer-vc projectile deadgrep ivy-prescient prescient counsel swiper ivy-rich ivy-hydra ivy yasnippet company helpful org smartparens-config all-the-icons major-mode-hydra eval-in-repl xr yasnippet-snippets outshine expand-region json-navigator mwim fish-mode sml-mode use-package-chords cmake-mode cask-mode eros macrostep org-indent validate hardhat beacon gitignore-mode gitconfig-mode manual use-package-hydra backup-each-save org-bullets ws-butlerz helm elisp-format comment-dwim-2 smex discover-my-major json-mode dired-toggle aggressive-indent gradle-mode groovy-imports groovy-mode ws-butler diminish key-chord)))
  '(python-shell-interpreter "python")
- '(text-mode-hook '(turn-on-flyspell set-word-wrap))
+ '(tabbar-mode nil nil (tabbar))
+ '(text-mode-hook (quote (set-word-wrap)))
  '(undo-limit 800000)
  '(visual-line-mode t t)
  '(word-wrap t))

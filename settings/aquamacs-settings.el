08e26d35daf8c5aedcabb7d0763c86b77dd983c6
Mark Bestley <37385+bestlem@users.noreply.github.com>
2020-10-28 12:36:25 +0000

    emacs 27 format

--- settings/aquamacs-settings.el
+++ settings/aquamacs-settings.el
@@ -13,4 +13,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aquamacs-variable-width ((t (:stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "Times New Roman")))))
+ '(aquamacs-variable-width
+   ((t
+     (:stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "Times New Roman")))))
--- settings/gnus-settings.el
+++ settings/gnus-settings.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(gnus-directory "~/Library/Preferences/Aquamacs Emacs/News")
  '(gnus-dribble-directory "~/tmp")
- '(gnus-group-sort-function '(gnus-group-sort-by-rank) t)
+ '(gnus-group-sort-function
+   (quote
+    (gnus-group-sort-by-rank))
+   t)
  '(gnus-kill-save-kill-file t)
  '(gnus-score-mimic-keymap nil)
  '(gnus-server-browse-in-group-buffer t)

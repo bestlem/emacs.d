f871e7d1797247edc94e19a70351808ba3c41b70
Mark Bestley <37385+bestlem@users.noreply.github.com>
2020-12-03 18:22:56 +0000

    Need early-init as well

--- early-init.el
+++ early-init.el
@@ -4,14 +4,14 @@
 (defvar file-name-handler-alist-old file-name-handler-alist)
 
 (setq file-name-handler-alist nil
-	  message-log-max 16384
-	  gc-cons-threshold 402653184
-	  gc-cons-percentage 0.6
-	  auto-window-vscroll nil)
+      message-log-max 16384
+      gc-cons-threshold 402653184
+      gc-cons-percentage 0.6
+      auto-window-vscroll nil)
 ;; org_mark_2020-01-23T20-40-42+00-00_mini12_EC7FD451-253D-4F87-90DC-AD484305487F ends here
 
 ;; [[file:init.org::org_mark_2020-09-28T01-08-28+01-00_mini12.local_407877DD-DF72-4900-9475-7723E8D855A2][org_mark_2020-09-28T01-08-28+01-00_mini12.local_407877DD-DF72-4900-9475-7723E8D855A2]]
-(setq package-enable-at-startup nil)
+;;(setq package-enable-at-startup nil)
 ;; org_mark_2020-09-28T01-08-28+01-00_mini12.local_407877DD-DF72-4900-9475-7723E8D855A2 ends here
 
 ;; [[file:init.org::org_mark_2020-09-28T01-08-28+01-00_mini12.local_0E90CFE6-BBAC-4DA3-8461-12811764098F][org_mark_2020-09-28T01-08-28+01-00_mini12.local_0E90CFE6-BBAC-4DA3-8461-12811764098F]]

34f321e733e58d06c71f840f86367b7de7922f68
Mark Bestley <37385+bestlem@users.noreply.github.com>
2020-12-06 11:59:40 +0000

    Remove face defines

--- custom.el
+++ custom.el
@@ -7,32 +7,3 @@
  '(pdf-view-midnight-colors
    '("#ffffff" . "#110b11"))
  '(treemacs-silent-filewatch t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default
-    ((t
-      (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "JetBrains Mono"))))
- '(bold-italic
-   ((t
-     (:underline t :slant italic :weight bold :family "Menlo"))))
- '(echo-area
-   ((t
-     (:inherit default :strike-through nil :underline nil :slant normal :weight normal :width normal))))
- '(fixed-pitch
-   ((t
-     (:family "JetBrains Mono"))))
- '(highlight
-   ((t
-     (:background "black" :foreground "white"))))
- '(italic
-   ((t
-     (:inherit default :slant italic))))
- '(region
-   ((t
-     (:extend t :background "deep pink" :foreground "white" :weight bold))))
- '(variable-pitch
-   ((t
-     (:weight normal :height 1.2 :family "Times New Roman")))))

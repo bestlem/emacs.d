d79deeb1acd403dbe9d5c4423c29b9893bc1ff98
Mark Bestley <37385+bestlem@users.noreply.github.com>
2020-12-08 21:40:07 +0000

    Modus only themes for non aqua

--- settings/modus-settings.el
+++ settings/modus-settings.el
@@ -3,10 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(modus-themes-completions 'opinionated)
+ '(modus-themes-fringes 'intense)
+ '(modus-themes-headings
+   ''((t)))
  '(modus-themes-mode-line '3d)
  '(modus-themes-org-blocks 'rainbow)
  '(modus-themes-paren-match 'intense-bold)
- '(modus-themes-prompts 'subtle)
+ '(modus-themes-prompts 'intense)
  '(modus-themes-scale-headings t)
  '(modus-themes-slanted-constructs t)
  '(modus-themes-syntax 'alt-syntax-yellow-comments)

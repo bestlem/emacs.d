8134c4cfe17a365622c87ec58331015ac65e1e9b
Mark Bestley <git@bestley.co.uk>
2021-01-21 00:14:22 +0000

    Update gnus directories to remove from root

---
+++ settings/gnus-settings.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(gnus-directory "~/Library/Preferences/Aquamacs Emacs/News")
+ '(gnus-home-directory "~/.emacs.d/var/gnus")
+ '(gnus-interactive-exit nil)
+ '(mail-default-directory "~/.emacs.d/varMail")
+ '(message-auto-save-directory "~/.emacs.d/var/gnus/Mail")
+ '(message-directory "~/.emacs.d/var/Mail/")
+ '(nnmail-message-id-cache-file "~/.emacs.d/var/gnus/.nnmail-cache"))

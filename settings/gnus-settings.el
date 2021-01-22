b42786e231d084b1c0a38141f1470949c6b2f99d
Mark Bestley <git@bestley.co.uk>
2021-01-22 13:59:23 +0000

    More gnus

--- settings/gnus-settings.el
+++ settings/gnus-settings.el
@@ -3,9 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(gnus-article-skip-boring t)
  '(gnus-directory "~/Library/Preferences/Aquamacs Emacs/News")
  '(gnus-home-directory "~/.emacs.d/var/gnus")
  '(gnus-interactive-exit nil)
+ '(gnus-posting-styles
+   '((".*"
+      (signature "Mark"))))
+ '(gnus-suppress-duplicates t)
  '(mail-default-directory "~/.emacs.d/varMail")
  '(message-auto-save-directory "~/.emacs.d/var/gnus/Mail")
  '(message-directory "~/.emacs.d/var/Mail/")

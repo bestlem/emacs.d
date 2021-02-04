951503805b74eb77d1ec8877bb29b6248dc056cc
Mark Bestley <git@bestley.co.uk>
2021-02-04 12:56:35 +0000

    Customization cleanup

--- custom.el
+++ custom.el
@@ -3,6 +3,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(canlock-password "8ac28fef8ff386f996bee4f20f8b3c40bd1829d4")
- '(imenu-sort-function 'imenu--sort-by-name)
- '(python-indent-guess-indent-offset nil))
+ )
--- settings/gnus-settings.el
+++ settings/gnus-settings.el
@@ -5,13 +5,17 @@
  ;; If there is more than one, they won't work right.
  '(gnus-article-skip-boring t)
  '(gnus-directory "~/Library/Preferences/Aquamacs Emacs/News")
+ '(gnus-group-line-format "%M%S%p%P%5y:%B%L %(%g%)
+")
+ '(gnus-group-sort-function
+   '(gnus-group-sort-by-level))
  '(gnus-home-directory "~/.emacs.d/var/gnus")
  '(gnus-interactive-exit nil)
  '(gnus-posting-styles
    '((".*"
-      (signature "Mark"))))
+	  (signature "Mark"))))
  '(gnus-suppress-duplicates t)
- '(mail-default-directory "~/.emacs.d/varMail")
+ '(mail-default-directory "~/.emacs.d/var/Mail")
  '(message-auto-save-directory "~/.emacs.d/var/gnus/Mail")
  '(message-directory "~/.emacs.d/var/Mail/")
  '(nnmail-message-id-cache-file "~/.emacs.d/var/gnus/.nnmail-cache"))

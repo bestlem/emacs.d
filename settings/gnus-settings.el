cb39b1b2fd9b427920112baab21488fa80b4418d
Mark Bestley <37385+bestlem@users.noreply.github.com>
2021-02-13 12:05:52 +0000

    Gnus updates - mainly for different from per newsgroup

--- settings/gnus-settings.el
+++ settings/gnus-settings.el
@@ -5,14 +5,15 @@
  ;; If there is more than one, they won't work right.
  '(gnus-article-skip-boring t)
  '(gnus-directory "~/Library/Preferences/Aquamacs Emacs/News")
- '(gnus-group-line-format "%M%S%p%P%5y:%B%L %(%g%)
-")
  '(gnus-group-sort-function
    '(gnus-group-sort-by-level))
  '(gnus-home-directory "~/.emacs.d/var/gnus")
  '(gnus-interactive-exit nil)
  '(gnus-posting-styles
    '((".*"
+	  (signature "Mark"))
+	 ("bar\\.baen\\.com"
+	  (address "baen@bestley.co.uk")
 	  (signature "Mark"))))
  '(gnus-suppress-duplicates t)
  '(mail-default-directory "~/.emacs.d/var/Mail")

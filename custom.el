343bcdb2eb5b194919a65b7e1b30ebf5595c6e8f
Mark Bestley <37385+bestlem@users.noreply.github.com>
2021-05-27 19:57:04 +0100

    On the way to working python

--- custom.el
+++ custom.el
@@ -3,4 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(canlock-password "5bc496f914e12916cdaf3640fdc97aa6b23f0fe3"))
+ '(canlock-password "5bc496f914e12916cdaf3640fdc97aa6b23f0fe3")
+ '(py-comment-auto-fill-p t)
+ '(py-company-pycomplete-p nil)
+ '(py-electric-comment-p t)
+ '(py-empty-line-closes-p t)
+ '(py-execute-no-temp-p nil)
+ '(py-imenu-show-method-args-p t)
+ '(py-smart-indentation t)
+ '(py-split-window-on-execute t)
+ '(py-split-windows-on-execute-function 'split-window-horizontally)
+ '(py-tab-shifts-region-p t))

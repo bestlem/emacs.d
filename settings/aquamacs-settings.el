aac4958a3ab06afb10ff7019543fd65866a59eed
Mark Bestley <37385+bestlem@users.noreply.github.com>
2021-07-07 12:20:18 +0100

    Make help work
    Bind keys after helpful loads
    Put all site-lisp on load-path
    Hide org properties

--- settings/aquamacs-settings.el
+++ settings/aquamacs-settings.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(aquamacs-variable-width
    ((t
-     (:stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "Times New Roman")))))
+	 (:stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "Times New Roman")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,7 +13,8 @@
  ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 312 t)
- '(aquamacs-save-options-on-quit 'ask)
+ '(aquamacs-save-options-on-quit
+   (quote ask))
  '(aquamacs-tool-bar-user-customization nil t)
  '(auto-word-wrap-default-function nil)
  '(tabbar-mode nil nil

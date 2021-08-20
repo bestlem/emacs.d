18bbbe4b6bb3968ed6114c7917831799f05280bb
Mark Bestley <37385+bestlem@users.noreply.github.com>
2021-08-20 20:42:20 +0100

    Update keys and show how to change a minor-mode key

--- custom.el
+++ custom.el
@@ -12,5 +12,23 @@
  '(py-imenu-show-method-args-p t)
  '(py-smart-indentation t)
  '(py-split-window-on-execute t)
- '(py-split-windows-on-execute-function 'split-window-horizontally)
+ '(py-split-windows-on-execute-function
+   (quote split-window-horizontally))
  '(py-tab-shifts-region-p t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(git-timemachine-minibuffer-author-face
+   ((t
+	 (:inherit success))))
+ '(git-timemachine-minibuffer-detail-face
+   ((t
+	 (:inherit warning))))
+ '(transient-posframe
+   ((t
+	 (:inherit tooltip))))
+ '(transient-posframe-border
+   ((t
+	 (:background "#d0a070")))))

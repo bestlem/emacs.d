acf00a816e4020578842ef8f51d760aca03111d1
Mark Bestley <37385+bestlem@users.noreply.github.com>
2022-06-13 21:07:37 +0100

    Note that canlock-password is part of gnus

--- settings/gnus-settings.el
+++ settings/gnus-settings.el
@@ -3,11 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(canlock-password "5bc496f914e12916cdaf3640fdc97aa6b23f0fe3")
  '(gnus-article-skip-boring t)
  '(gnus-directory "~/Library/Preferences/Aquamacs Emacs/News")
  '(gnus-group-sort-function
-   '(gnus-group-sort-by-level)
-   t)
+   '(gnus-group-sort-by-level))
  '(gnus-home-directory "~/.emacs.d/var/gnus")
  '(gnus-interactive-exit nil)
  '(gnus-posting-styles

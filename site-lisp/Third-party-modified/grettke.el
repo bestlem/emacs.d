c87140fa44d97926e110d542c4caf6029942363a
Mark Bestley <37385+bestlem@users.noreply.github.com>
2022-06-19 13:19:36 +0100

    First version of grettke.el - Unused

---
+++ site-lisp/Third-party-modified/grettke.el
@@ -0,0 +1,167 @@
+;;; grettke ---  Functions from  grettke -*- lexical-binding: t -*-
+
+;; Copyright Grant Rettke
+;; Version: 0.1
+;; Package-Requires: (dependencies)
+
+;; Keywords: keywords
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;
+(require 'org)
+;;; Code:
+
+(defun help/display-system-info ()
+  (interactive)
+  (message "<<<Org Information>>>\nThis buffer file: %s\nAs Of: %s\nOrg-Version: %s\nOrg-Git-Version:%s\nEmacs-Version: %s\nNoweb wrap start and stop delimeters: '%s' and '%s'\norg-babel-default-header-args:\n"
+           buffer-file-name
+           (help/get-timestamp)
+           (org-version)
+           (org-git-version)
+           (emacs-version)
+           org-babel-noweb-wrap-start
+           org-babel-noweb-wrap-end)
+  (pp org-babel-default-header-args))
+
+(defun help/set-org-babel-default-header-args (property value)
+  "Easily set system header arguments in org mode.
+
+PROPERTY is the system-wide value that you would like to modify.
+
+VALUE is the new value you wish to store.
+
+Attribution: URL `http://orgmode.org/manual/System_002dwide-header-arguments.html#System_002dwide-header-arguments'"
+  (setq org-babel-default-header-args
+        (cons (cons property value)
+              (assq-delete-all property org-babel-default-header-args))))
+
+(defun help/set-org-babel-default-inline-header-args (property value)
+  "See `help/set-org-babel-default-header-args'; same but for inline header args."
+  (setq org-babel-default-inline-header-args
+        (cons (cons property value)
+              (assq-delete-all property org-babel-default-inline-header-args))))
+
+(defun help/set-org-babel-default-header-args:R (property value)
+  "See `help/set-org-babel-default-header-args'; same but for R.
+
+This is a copy and paste. Additional languages would warrant a refactor."
+  (setq org-babel-default-header-args:R
+        (cons (cons property value)
+              (assq-delete-all property org-babel-default-header-args:R))))
+
+(defun help/set-org-babel-default-header-args:ditaa (property value)
+  "See `help/set-org-babel-default-header-args'; same but for ditaa.
+
+This is a copy and paste. Additional languages would warrant a refactor."
+  (setq org-babel-default-header-args:ditaa
+        (cons (cons property value)
+              (assq-delete-all property org-babel-default-header-args:ditaa))))
+
+(defun help/set-org-babel-default-header-args:dot (property value)
+  "See `help/set-org-babel-default-header-args'; same but for dot.
+
+This is a copy and paste. Additional languages would warrant a refactor."
+  (setq org-babel-default-header-args:dot
+        (cons (cons property value)
+              (assq-delete-all property org-babel-default-header-args:dot))))
+
+(defun help/set-org-babel-default-header-args:plantuml (property value)
+  "See `help/set-org-babel-default-header-args'; same but for plantuml.
+
+This is a copy and paste. Additional languages would warrant a refactor."
+  (setq org-babel-default-header-args:plantuml
+        (cons (cons property value)
+              (assq-delete-all property org-babel-default-header-args:plantuml))))
+
+(defun help/org-toggle-macro-markers ()
+  (interactive)
+  (let ((old org-hide-macro-markers)
+        (new (not org-hide-macro-markers)))
+    (setq org-hide-macro-markers new)
+    (message "Just changed org-hide-macro-markers from %s to %s" old new)
+    (font-lock-mode)
+    (font-lock-mode)))
+
+(defun help/org-prp-hdln ()
+  "Visit every Headline. If it doesn't have an ID property then add one and
+  assign it a UUID. Attribution: URL
+  `http://article.gmane.org/gmane.emacs.orgmode/99738'. It is OK to leave the
+  colon separator in here because these are never used as Source-Blocks and
+  the rest of the code expects the colon separator."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (dolist (p (nreverse
+                (org-element-map (org-element-parse-buffer 'headline) 'headline
+                  (lambda (headline) (org-element-property :begin headline)))))
+      (goto-char p)
+      (org-id-get-create))
+    (save-buffer)))
+
+(defun help/org-id-new ()
+  "Re-purposing `org-id' hit a snag when colons were forbidden in Source-Block
+  names. Adding support for a user-defined Org-Id separator would have fixed
+  this but with no benefit to Org-Id. So this function removes the colon
+  instead.
+ "
+  (interactive)
+  (let* ((gend (org-id-new))
+         (newid (replace-regexp-in-string ":" "_" gend)))
+    newid))
+
+(defun help/org-prp-src-blk ()
+  "If it doesn't have a NAME property then add one and
+   assign it a UUID. Attribution: URL `http://article.gmane.org/gmane.emacs.orgmode/99740'"
+  (interactive)
+  (help/org-2every-src-block
+   #'(lambda (element)
+       (if (not (org-element-property :name element))
+           (let ((i (org-get-indentation)))
+            (beginning-of-line)
+            (save-excursion (insert "#+NAME: " (help/org-id-new) "\n"))
+            (indent-to i)
+            (forward-line 2))))))
+
+(defconst help/org-special-pre "^\s*#[+]")
+
+(defun help/org-2every-src-block (fn)
+  "Visit every Source-Block and evaluate `FN'."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((case-fold-search t))
+      (while (re-search-forward (concat help/org-special-pre "BEGIN_SRC") nil t)
+        (let ((element (org-element-at-point)))
+          (when (eq (org-element-type element) 'src-block)
+            (funcall fn element)))))
+    (save-buffer)))
+
+(defun help/org-babel-demarcate-block ()
+  "Add a NAME property then assign it a UUID."
+  (interactive)
+  (org-babel-demarcate-block)
+  (insert "#+name: " (help/org-id-new))
+  (beginning-of-line)
+  (insert "\n"))
+
+(provide 'grettke)
+
+;;; grettke.el ends here

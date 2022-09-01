1fd53745ad6da3e57c5963efe983559da66c8691
Mark Bestley <37385+bestlem@users.noreply.github.com>
2022-09-01 12:10:44 +0100

    WIP elpaca alt loads - but only for one version due to how packages are built

--- alt/elpaca.el
+++ alt/elpaca.el
@@ -1,24 +1,37 @@
 ;;; init-elpaca.el --- ELPACA INIT DEMO   -*- lexical-binding: t; -*-
 ;; Bootstrap Elpaca
 (declare-function elpaca-generate-autoloads "elpaca")
-(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-directory (expand-file-name "elpaca/" mwb-emacs-work-dir))
+
+(defvar elpaca-mwb-build-dir (expand-file-name
+							  (concat "build-" emacs-version "/elpaca")
+							  elpaca-directory))
+
+;; (unless (file-directory-p elpaca-mwb-build-dir)
+;;   (make-directory elpaca-mwb-build-dir t))
+
+
 (when-let ((elpaca-repo (expand-file-name "repos/elpaca/" elpaca-directory))
-           (elpaca-build (expand-file-name "builds/elpaca/" elpaca-directory))
+					;(elpaca-build elpaca-mwb-build-dir)
+	   (elpaca-build (expand-file-name "builds/elpaca/" elpaca-directory))
            (elpaca-target (if (file-exists-p elpaca-build) elpaca-build elpaca-repo))
            (elpaca-url  "https://www.github.com/progfolio/elpaca.git")
            ((add-to-list 'load-path elpaca-target))
            ((not (file-exists-p elpaca-repo)))
            (buffer (get-buffer-create "*elpaca-bootstrap*")))
+  (message "In bootstrap with target %s" elpaca-target)
   (condition-case-unless-debug err
       (progn
         (unless (zerop (call-process "git" nil buffer t "clone" elpaca-url elpaca-repo))
           (error "%s" (list (with-current-buffer buffer (buffer-string)))))
         (byte-recompile-directory elpaca-repo 0 'force)
         (require 'elpaca)
+		(message "generating autoloads")
         (elpaca-generate-autoloads "elpaca" elpaca-repo)
         (kill-buffer buffer))
     ((error)
      (delete-directory elpaca-directory 'recursive)
+	 (message "In error")
      (with-current-buffer buffer
        (goto-char (point-max))
        (insert (format "\n%S" err))
